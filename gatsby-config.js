759a44e216db4c4ed92f91a7a6dc7359a6652489
Menno Buggenhout <mennobuggenhout@gmail.com>
2021-05-10 00:01:43 +0200

    sqd

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,13 @@ module.exports = {
   /* Your site config here */
   plugins: [
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
   ],
 }
